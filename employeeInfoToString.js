7530be81adaf6c5632b5d935e02418e76f4e1cce
Rostyslav Harasymiak <rharasymiak@icstars.org>
2024-08-09 18:40:58 -0500

    Added explanations

--- employeeInfoToString.js
+++ employeeInfoToString.js
@@ -5,12 +5,12 @@
 
 export function employeeInfoToString(employee) {
     
-    let full_name = generateFullName(employee.first_name, employee.middle_name, employee.last_name);
-    return `${full_name}\nDepartment: ${employee.department} | Salary: ${employee.salary}`;
+    let full_name = generateFullName(employee.first_name, employee.middle_name, employee.last_name);   // here I noticed the argument #3 was wrong so I fixed it
+    return `${full_name}\nDepartment: ${employee.department} | Salary: ${employee.salary}`;    // here I noticed we missing "\n" so I fixed it
     
 }
 
 function generateFullName(first_name, middle_name, last_name) {
 
-    return `${first_name}${middle_name != null && middle_name !== "" ? " " + middle_name + " " : " " }${last_name}`;
+    return `${first_name}${middle_name != null && middle_name !== "" ? " " + middle_name + " " : " " }${last_name}`;     //here I added  && middle_name !== "" to expresion to check if middle_name is not just null but also not empty string
 }

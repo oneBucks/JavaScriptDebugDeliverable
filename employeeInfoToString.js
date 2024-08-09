0dc312c1c3e2e683d68798b90743ea4a2fd4457b
Rostyslav Harasymiak <rharasymiak@icstars.org>
2024-08-09 09:29:44 -0500

    Fixed one

--- employeeInfoToString.js
+++ employeeInfoToString.js
@@ -4,12 +4,13 @@
 // Hint: using a console.log may be useful...
 
 export function employeeInfoToString(employee) {
-    let full_name = generateFullName(employee.first_name, employee.first_name, employee.last_name);
-
-    return `${full_name}
-     Department: ${employee.department} | Salary:${employee.salary}`;
+    
+    let full_name = generateFullName(employee.first_name, employee.middle_name, employee.last_name);
+    return `${full_name}\nDepartment: ${employee.department} | Salary: ${employee.salary}`;
+    
 }
 
 function generateFullName(first_name, middle_name, last_name) {
-    return `${first_name}${middle_name != null ? " " + middle_name + " " : " " }${last_name}`
+
+    return `${first_name}${middle_name != null && middle_name !== "" ? " " + middle_name + " " : " " }${last_name}`;
 }
